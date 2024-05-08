796c6d84993fc27536705336b6776aa1667f733d
Katherine L. Bottenhorn <kbott006@fiu.edu>
2024-05-08 16:09:17 -0500

    oops forgot manifest and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='abcdWrangler',
+    version='0.0.1',
+    author='Katie Bottenhorn',
+    description='A package for wrangling ABCD Study data, for various data science tasks.',
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True
+)
