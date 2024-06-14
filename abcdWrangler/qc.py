7047b6a977d15d0d565a242b28d4395a1e4f143a
Katherine L. Bottenhorn <kbott006@fiu.edu>
2024-06-14 11:16:28 -0700

    update mri qc fxns for long and wide and multiindex

--- abcdWrangler/qc.py
+++ abcdWrangler/qc.py
@@ -23,13 +23,20 @@ def smri_qc(df):
         Dataframe: participants x variables. If `long=True`, and `eventname` is a list, 
         then `participants` are index level 0 and `eventname` is index level 1.
     '''
-    # exclude ppl with abnormal findings
-    smri_mask = df['mrif_score'].between(1,2, inclusive='both')
+    # include ppl without abnormal findings
+    mrif_columns = df.filter(like='mrif_score').columns
     # t1 quality for freesurfer ROI delineations
     # 1=include, 0=exclude
-    smri_mask *= df['imgincl_t1w_include'] == 1
-    smri_mask = np.invert(smri_mask)
-    #ppts = df.loc[smri_mask == True].index
+    incl_columns = df.filter(like='imgincl_t1w_include').columns
+    smri_mask = df[mrif_columns[0]].between(1,2, inclusive='both')
+    smri_mask *= df[incl_columns[0]] == 1
+    if len(mrif_columns) > 1:
+        for col in mrif_columns[1:]:
+            smri_mask *= df[col].between(1,2, inclusive='both')
+    if len(incl_columns) > 1:
+        for col in incl_columns[1:]:
+            smri_mask *= df[col] == 1
+    ppts = df.loc[smri_mask == True].index
     return ppts
 
 
@@ -46,29 +53,78 @@ def dmri_qc(df, motion_thresh=False):
         Dataframe: participants x variables. If `long=True`, and `eventname` is a list, 
         then `participants` are index level 0 and `eventname` is index level 1.
     '''
-    dmri_mask = df['mrif_score'].between(1,2, inclusive='both')
-    # dmri quality for RSI estimates
+    mrif_columns = df.filter(like='mrif_score').columns
+    # t1 quality for freesurfer ROI delineations
     # 1=include, 0=exclude
-    # head motion greater than 2mm FD on average = exclude
-    dmri_mask *= df['imgincl_dmri_include'] == 1
+    incl_columns = df.filter(like='imgincl_dmri_include').columns
+    dmri_mask = df[mrif_columns[0]].between(1,2, inclusive='both')
+    dmri_mask *= df[incl_columns[0]] == 1
+    if len(mrif_columns) > 1:
+        for col in mrif_columns[1:]:
+            dmri_mask *= df[col].between(1,2, inclusive='both')
+    if len(incl_columns) > 1:
+        for col in incl_columns[1:]:
+            dmri_mask *= df[col] == 1
     if motion_thresh:
-        dmri_mask *= df['dmri_meanmotion'] < motion_thresh
+        motion_columns = df.filter(like='dmri_meanmotion').columns
+        dmri_mask *= df[motion_columns[0]] < motion_thresh
+        if len(motion_columns) > 1:
+            for col in motion_columns[1:]:
+                dmri_mask *= df[col] < motion_thresh
     #dmri_mask = np.invert(dmri_mask)
     ppts = df.loc[dmri_mask == True].index
     return ppts
 
-def fmri_qc(df, ntpoints=500, motion_thresh=1):
+def fmri_qc(df, ntpoints=None, motion_thresh=1):
     '''
     '''
-    rsfmri_mask = df['mrif_score'].between(1,2, inclusive='both') * df['imgincl_rsfmri_include'] == 1
-    # iteratively build a mask using boolean ANDs 
-    # included ppts meet all the specified criteria
-    if ntpoints:
-        rsfmri_mask *= df['rsfmri_ntpoints'] >= ntpoints
-        #rsfmri_mask *= rsfmri_mask2
+    mrif_columns = df.filter(like='mrif_score').columns
+    # t1 quality for freesurfer ROI delineations
+    # 1=include, 0=exclude
+    incl_columns = df.filter(like='imgincl_rsfmri_include').columns
+    fmri_mask = df[mrif_columns[0]].between(1,2, inclusive='both')
+    fmri_mask *= df[incl_columns[0]] == 1
+    if len(mrif_columns) > 1:
+        for col in mrif_columns[1:]:
+            fmri_mask *= df[col].between(1,2, inclusive='both')
+    if len(incl_columns) > 1:
+        for col in incl_columns[1:]:
+            fmri_mask *= df[col] == 1
     if motion_thresh:
-        rsfmri_mask *= df['rsfmri_meanmotion'] < motion_thresh
-        #rsfmri_mask *= rsfmri_mask2
-    #rsfmri_mask = np.invert(rsfmri_mask)
-    ppts = df.loc[rsfmri_mask == True].index
+        motion_columns = df.filter(like='rsfmri_meanmotion').columns
+        fmri_mask *= df[motion_columns[0]] < motion_thresh
+        if len(motion_columns) > 1:
+            for col in motion_columns[1:]:
+                fmri_mask *= df[col] < motion_thresh
+    if ntpoints:
+        tr_columns = df.filter(like='rsfmri_ntpoints').columns
+        fmri_mask *= df[tr_columns[0]] >= ntpoints
+        if len(tr_columns) > 1:
+            for col in tr_columns[1:]:
+                fmri_mask *= df[col] >= ntpoints
+    ppts = df.loc[fmri_mask == True].index
     return ppts
+
+def one_ppt_per_fam(family_dat):
+    '''
+    Function for randomly choosing one participant per family for analyses that cannot account for family dependencies.
+    Parameters
+    ----------
+    family_dat : Pandas Series
+        Series (single row from Pandas DataFrame) that has ppt IDs as rows and `rel_family_id` as the singular column
+    Returns
+    -------
+    ppts : list
+        List of participants that includes one randomly chosen PPT per family
+    '''
+    assert len(family_dat.columns) == 1
+    all_subj = family_dat.index
+    for id_ in family_dat.unique():
+        siblings = family_dat[family_dat == id_].index.to_list()
+        if len(siblings) > 1:
+            keep = np.random.choice(siblings)
+            siblings.remove(keep)
+            all_subj = list(set(all_subj) - set(siblings))
+        else:
+            pass
+    return all_subj
