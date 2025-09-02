1ace9058a03c34163e3d78fd4b788b85c7410fec
Katherine L. Bottenhorn <kbott006@fiu.edu>
2025-09-02 09:56:50 +0100

    change init imports

--- abcdWrangler/__init__.py
+++ abcdWrangler/__init__.py
@@ -1,5 +1,5 @@
 from .io import data_grabber
 #from .dataviz import assign_region_names, plot_brains, series_2_nifti
-from .qc import smri_qc, dmri_qc, fmri_qc
+from .qc import smri_qc, dmri_qc, rsfmri_qc, tfmri_qc
 
 #__all__ = []
